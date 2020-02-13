d54f7b1e880f7c9e3086579218e53f8d26a1ccfd
Kirill <kirill.aistrakhanov@walletfactory.com>
2020-02-13 08:10:55 +0200

    Hamburger overlay menu, accordion

---
+++ src/components/menu/Accordion.vue
@@ -0,0 +1,288 @@
+<template>
+    <div class="accordion">
+        <div
+                class="accordion-item"
+                v-for="(content, i) in contents"
+                v-bind:class="{ 'accordion-active': content.active }"
+        >
+            <div v-if="!content.link" class="accordion-header">
+                <a href="#" v-on:click="expand($event, i)">
+                    <div v-if="!content.link" class="accordion-header-div">{{ content.title }}</div>
+
+                    <div v-show="content.isExpandable" class="accordion-header-div">
+                        <div class="accordion-caret"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div  v-if="content.link" class="accordion-header">
+                <a href="#" @click="goToByName(content.link)">
+                    <div v-if="content.link"  class="accordion-header-div">{{ content.title }}</div>
+                    <div v-show="content.isExpandable" class="accordion-header-div">
+                        <div class="accordion-caret"></div>
+                    </div>
+                </a>
+            </div>
+
+
+            <div v-if="content.isExpandable" class="accordion-body" :ref="'accordion-body-' + i">
+                <div  class="accordion-content" v-for="(link, i) in content.description"  :key="i">
+                    <div v-if="!link.customCallback" @click="goToByName(link.routeName)" >{{ link.title }}</div>
+                    <div v-else @click="link.customCallback()" >{{ link.title }}</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+    import { TweenLite, Power2} from "gsap";
+
+    export default {
+
+        methods: {
+            goToByName(name) {
+                return this.$router.push({
+                    name: name
+                });
+            },
+            useMainNet() {
+                window.location = "/mainnet";
+            },
+            useBabylon() {
+                window.location = "/babylonnet";
+            },
+            useCarthage() {
+                window.location = "/carthage";
+            },
+
+            expand: function(e, i) {
+                console.log('CLICKED', this.$refs, i);
+
+
+                let el = this.$refs['accordion-body-' + i][0];
+
+                if(this.contents[i].isExpandable) {
+                    if (this.contents[i].active === false) {
+                        this.contents[i].active = true;
+
+                        TweenLite.to(el, .35, {
+                            height: el.scrollHeight,
+                            ease: Power2.easeIn
+                        });
+                    } else {
+                        this.contents[i].active = false;
+
+                        TweenLite.to(el, 0.25, {
+                            height: 0,
+                            ease: Power2.easeInOut
+                        });
+                    }
+                }
+
+
+            }
+        },
+
+
+        data() {
+        return {
+            contents: [
+                {
+                    title: 'Dashboard',
+                    active: false,
+                    isExpandable: false,
+                    link: 'index'
+                },
+                {
+                title: 'Blocks',
+                description: [
+                    {
+                        title: 'Blocks',
+                        routeName: 'blocks'
+                    },
+                    {
+                        title: 'Snapshots',
+                        routeName: 'snapshots'
+                    },
+                    {
+                        title: 'Baking Rights',
+                        routeName: 'baking_rights'
+                    }
+                ],
+                active: false,
+                isExpandable: true
+                },
+                {
+                    title: 'Current Network',
+                    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                    active: false,
+                    isExpandable: true,
+                    description: [
+                        {
+                            title: 'Mainnet',
+                            customCallback: this.useMainNet
+                        },
+                        {
+                            title: 'Babylon',
+                            customCallback: this.useBabylon
+                        },
+                        {
+                            title: 'Carthage',
+                            customCallback: this.useCarthage
+                        },
+                    ],
+                },
+            //     {
+            //     title: 'Duis aute irure dolor in reprehenderit',
+            //     description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+            //     active: false,
+            //     isExpandable: true
+            //
+            // }, {
+            //     title: 'Excepteur sint occaecat cupidatat non proident',
+            //     description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            //     active: false,
+            //     isExpandable: true
+            //
+            // }
+            ],
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    $base-color: #727272;
+    $base-width: 320px;
+    $base-time: 200ms;
+
+    $app-background-color: #82dfff;
+    $app-padding: 2rem;
+
+    $accordion-background-color: #ffffff;
+    $accordion-max-width: $base-width;
+    $accordion-padding: 2rem 1rem;
+    $accordion-border-radius: 0.5rem;
+    $accordion-box-shadow: 0;
+
+    $accordion-header-font-size: 14px;
+    $accordion-header-height: 2rem;
+    $accordion-header-padding: 1rem 1rem 1rem 1rem;
+    $accordion-header-border-radius: 0.6rem;
+    $accordion-header-transition: background-color $base-time ease-in-out;
+
+    $accordion-header-hover-background-color: #efefef;
+
+    $accordion-caret-color: $base-color;
+    $accordion-caret-background-image: linear-gradient(
+                    to top right,
+                    transparent 50%,
+                    $accordion-caret-color 50%
+    );
+    $accordion-caret-width: 0.5rem;
+    $accordion-caret-height: 0.5rem;
+    $accordion-caret-transform: rotate(-45deg);
+    $accordion-caret-transforming: scale(1.5) rotate(45deg);
+    $accordion-caret-active-transform: rotate(135deg);
+
+    $accordion-body-content-padding: 1rem 0rem 1rem 1.4rem;
+
+
+
+
+    .accordion {
+        background-color: $accordion-background-color;
+        width: 100%;
+        max-height: 100%;
+        max-width: $accordion-max-width;
+        padding: $accordion-padding;
+        margin: auto;
+        box-sizing: border-box;
+        overflow: auto;
+        box-shadow: $accordion-box-shadow;
+    }
+
+    .accordion-item.accordion-active {
+        .accordion-caret {
+            animation: accordion-is-active $base-time linear forwards;
+        }
+    }
+
+    .accordion-header {
+        color: inherit;
+        font-size: $accordion-header-font-size;
+        font-weight: normal;
+        position: relative;
+        a {
+            color: inherit;
+            text-decoration: none;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+            box-sizing: border-box;
+            border-radius: $accordion-header-border-radius;
+            transition: $accordion-header-transition;
+            &:not(.accordion-active):hover {
+                background-color: $accordion-header-hover-background-color;
+            }
+        }
+    }
+
+    .accordion-header-div {
+        padding: $accordion-header-padding;
+        &:last-child {
+            padding-left: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+
+    .accordion-caret {
+        background-image: $accordion-caret-background-image;
+        width: $accordion-caret-width;
+        height: $accordion-caret-height;
+        transform: $accordion-caret-transform;
+        animation: accordion-is-inactive $base-time linear forwards;
+    }
+
+    .accordion-body {
+        height: 0;
+        overflow: hidden;
+    }
+
+    .accordion-content {
+        padding: $accordion-body-content-padding;
+        margin: 0 0 0 1rem ;
+        justify-content: space-between;
+        box-sizing: border-box;
+        border-radius: $accordion-header-border-radius;
+        transition: $accordion-header-transition;
+        cursor: pointer;
+        &:not(.accordion-active):hover {
+            background-color: $accordion-header-hover-background-color;
+        }
+    }
+
+    @keyframes accordion-is-inactive {
+        0%   { transform: $accordion-caret-transform; }
+        50%  { transform: $accordion-caret-transforming; }
+        100% { transform: $accordion-caret-active-transform; }
+    }
+
+    @keyframes accordion-is-active {
+        0%   { transform: $accordion-caret-active-transform; }
+        50%  { transform: $accordion-caret-transforming; }
+        100% { transform: $accordion-caret-transform; }
+    }
+
+    @media screen and (min-width: $base-width) {
+        .accordion {
+            max-height: none;
+            border-radius: $accordion-border-radius;
+        }
+
+    }
+</style>
---
+++ src/components/menu/Overlay.vue
@@ -0,0 +1,273 @@
+<template>
+    <div>
+        <h3 @click="toggleMenu">
+            <font-awesome-icon  v-if="isOpen" class="icon" icon="times-circle" />
+            <font-awesome-icon   v-else  class="icon" icon="bars" />
+        </h3>
+        <div :class="{overlay: true, open: isOpen}" id="overlay">
+
+            <div class="overlay_container ">
+                <Accordion />
+                <div class="search search-wrapper-nav">
+                    <Search v-on:onSubmit="toggleMenu"/>
+                </div>
+                <Footer class="footer--fixed" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Search from "../search/search";
+    import Accordion  from "./Accordion";
+    import Footer from "./footer";
+
+    export default {
+        name: "OverlayMenu",
+        components: { Search, Accordion, Footer },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            toggleMenu(){
+                this.isOpen = !this.isOpen;
+            }
+        },
+        watch: {
+            $route() {
+                this.isOpen = false
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    $color-background: #F5F5F5;
+    $color-main: #fff;
+    $color-active: #FFF;
+    $color-link: #FFF;
+
+    $button-height: 27px;
+    $button-width: 35px;
+
+
+    .icon {
+        cursor: pointer;
+    }
+    .container {
+        position: absolute;
+        width: 100%;
+        heigh: 100%;
+        text-align: center;
+        top: 40%;
+        left: 0;
+        margin:  0 auto;
+        font-family: 'Varela Round', sans-serif;
+
+    p {
+        font-size: 20px;
+    }
+
+    a {
+        display: inline-block;
+        position: relative;
+        text-align: center;
+        color: $color-main;
+        text-decoration: none;
+        font-size: 20px;
+        overflow: hidden;
+        top: 5px;
+
+    &:after {
+         content: '';
+         position: absolute;
+         background: $color-main;
+         height: 2px;
+         width: 0%;
+         transform: translateX(-50%);
+         left: 50%;
+         bottom: 0;
+         transition: .35s ease;
+     }
+    &:hover:after,
+    &:focus:after,
+    &:active:after {
+         width: 100%;
+     }
+    }
+    }
+
+    h1 {
+        position: relative;
+        text-align: center;
+        font-family: 'Varela Round', serif;
+    }
+
+    .button_container {
+        position: fixed;
+        top: 5%;
+        right: 2%;
+        height: $button-height;
+        width: $button-width;
+        cursor: pointer;
+        z-index: 100;
+        transition: opacity .25s ease;
+
+    &:hover {
+         opacity: .7;
+     }
+
+    &.active {
+    .top {
+        transform: translateY(11px) translateX(0) rotate(45deg);
+        background: $color-active;
+    }
+    .middle {
+        opacity: 0;
+        background: $color-active;
+    }
+
+    .bottom {
+        transform: translateY(-11px) translateX(0) rotate(-45deg);
+        background: $color-active;
+    }
+    }
+
+    span {
+        background: $color-main;
+        border: none;
+        height: 5px;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transition:  all .35s ease;
+        cursor: pointer;
+
+    &:nth-of-type(2) {
+         top: 11px;
+     }
+
+    &:nth-of-type(3) {
+         top: 22px;
+     }
+    }
+    }
+
+    .overlay {
+        top: 60px; // Header height
+        position: fixed;
+        background: $color-main;
+        left: 0;
+        width: 100%;
+        height: 0%;
+        opacity: 1;
+        visibility: hidden;
+        transition: opacity .35s, visibility .35s, height .35s;
+        overflow: hidden;
+        z-index: 999;
+
+    &.open {
+         top: 60px;
+         opacity: 1;
+         visibility: visible;
+         height: 100%;
+
+    li {
+        animation: fadeInRight .5s ease forwards;
+        animation-delay: .35s;
+
+    &:nth-of-type(2) {
+         animation-delay: .4s;
+     }
+    &:nth-of-type(3) {
+         animation-delay: .45s;
+     }
+    &:nth-of-type(4) {
+         animation-delay: .50s;
+     }
+    }
+    }
+    nav {
+        position: relative;
+        height: 70%;
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: 50px;
+        font-weight: 400;
+        text-align: center;
+    }
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0 auto;
+        display: inline-block;
+        position: relative;
+        height: 100%;
+
+    li {
+        display: block;
+        height: 25%;
+        height: calc(100% / 4);
+        min-height: 50px;
+        position: relative;
+        opacity: 0;
+
+    a {
+        display: block;
+        position: relative;
+        color: $color-link;
+        text-decoration: none;
+        overflow: hidden;
+
+    &:hover:after,
+    &:focus:after,
+    &:active:after {
+         width: 100%;
+     }
+
+    &:after {
+         content: '';
+         position: absolute;
+         bottom: 0;
+         left: 50%;
+         width: 0%;
+         transform: translateX(-50%);
+         height: 3px;
+         background: $color-link;
+         transition: .35s;
+     }
+    }
+    }
+    }
+    }
+    @keyframes fadeInRight {
+        0% {
+            opacity: 0;
+            left: 20%;
+        }
+        100% {
+            opacity: 1;
+            left: 0;
+        }
+    }
+
+    .footer--fixed {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+
+    .search-wrapper-nav {
+        width: 80%;
+        margin: auto;
+        input {
+            height: 60px !important;
+        }
+        button {
+            height: 60px !important;
+        }
+    }
+</style>
