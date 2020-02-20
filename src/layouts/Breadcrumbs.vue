0ec9872ccdb03b1b71ce6a8f8582986b58018a71
shotgunofdeath <faust.child@gmail.com>
2020-02-20 15:22:11 +0200

    feat(page container): - PageContentContainer.vue and Breadcrumbs.vue are added - StatisticsCard.vue is added

---
+++ src/layouts/Breadcrumbs.vue
@@ -0,0 +1,31 @@
+<template>
+  <ol class="breadcrumb ml-2">
+    <li
+      v-for="(crumb, index) in crumbs"
+      :key="index"
+      :class="{
+        'breadcrumb-item': true,
+        active: index === crumbs.length - 1
+      }"
+    >
+      <router-link
+        :to="{ name: crumb.toRouteName }"
+        :tag="index === crumbs.length - 1 ? 'span' : 'a'"
+      >
+        {{ crumb.text }}
+      </router-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: "Breadcrumbs",
+  props: {
+    crumbs: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
