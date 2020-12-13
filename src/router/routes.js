387c9af28938d6db871f3e2409ae57b41e4237ad
muratkarsh <muratkarsh@gmail.com>
2020-12-14 00:04:36 +0200

    refactor(router): update routes with necessary middlewares

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
-import { translation } from '@/plugins/translation';
-import { state as applicationState } from '@/store/modules/app.module';
-
 const PageContentContainer = () => import('@/layouts/PageContentContainer');
 const Index = () => import('../views/Index');
+
+// Middlewares
+import language from '@/router/middleware/language';
+import network from '@/router/middleware/network';
+import page from '@/router/middleware/page';
 
 /* Blocks group */
 // Blocks
@@ -76,17 +78,11 @@ export default [
   {
     path: '/',
     name: 'index',
-    redirect() {
-      const userLanguage = translation.getUserLang().langNoISO;
-      return `${userLanguage}/${applicationState.network}`;
-    },
+    redirect: { name: 'network' },
   },
   {
     path: '/:language',
-    redirect() {
-      const userLanguage = translation.getUserLang().langNoISO;
-      return `${userLanguage}/${applicationState.network}`;
-    },
+    redirect: { name: 'network' },
   },
   {
     path: '/:language/:network',
@@ -96,100 +92,198 @@ export default [
         path: '',
         name: 'network',
         component: Index,
+        meta: {
+          middleware: [language, network],
+        },
       },
       /* Blocks group */
       {
         name: 'blocks',
         path: 'blocks/:page?',
         component: Blocks,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       {
         name: 'block',
         path: 'block/:level',
         component: Block,
+        meta: {
+          middleware: [language, network],
+        },
       },
       {
         name: 'snapshots',
         path: 'snapshots/:page?',
         component: Snapshots,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       {
         name: 'baking_rights',
         path: 'baking_rights',
         component: BakingRights,
+        meta: {
+          middleware: [language, network],
+        },
       },
       /* Blocks group end */
 
       /* Operations group */
       // Txs
-      { path: 'txs/:page?', name: 'txs', component: Txs },
-      { path: 'tx/:txhash', name: 'tx', component: Tx },
+      {
+        path: 'txs/:page?',
+        name: 'txs',
+        component: Txs,
+        meta: {
+          middleware: [language, network, page],
+        },
+      },
+      {
+        path: 'tx/:txhash',
+        name: 'tx',
+        component: Tx,
+        meta: {
+          middleware: [language, network],
+        },
+      },
       // Endorsements
       {
         path: 'endorsements/:page?',
         name: 'endorsements',
         component: Endorsements,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Delegations
       {
         path: 'delegations/:page?',
         name: 'delegations',
         component: Delegations,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Originations
       {
         path: 'originations/:page?',
         name: 'originations',
         component: Originations,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Activations
       {
         path: 'activations/:page?',
         name: 'activations',
         component: Activations,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Double baking
       {
         path: 'double_baking/:page?',
         name: 'double_baking',
         component: DoubleBaking,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Double endorsement
       {
         path: 'double_endorsement/:page?',
         name: 'double_endorsement',
         component: DoubleEndorsement,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       /* Operations group end */
 
       // Bakers
-      { path: 'bakers/:page?', name: 'bakers', component: Bakers },
+      {
+        path: 'bakers/:page?',
+        name: 'bakers',
+        component: Bakers,
+        meta: {
+          middleware: [language, network, page],
+        },
+      },
       {
         path: 'public_bakers/:page?',
         name: 'public_bakers',
         component: BakersPublic,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
-      { path: 'baker/:baker', name: 'baker', component: Baker },
+      {
+        path: 'baker/:baker',
+        name: 'baker',
+        component: Baker,
+        meta: {
+          middleware: [language, network],
+        },
+      },
 
       /* Accounts group */
       // Accounts
-      { path: 'accounts/:page?', name: 'accounts', component: Accounts },
+      {
+        path: 'accounts/:page?',
+        name: 'accounts',
+        component: Accounts,
+        meta: {
+          middleware: [language, network, page],
+        },
+      },
       {
         path: 'account/:account',
         name: 'account',
         component: Account,
+        meta: {
+          middleware: [language, network],
+        },
       },
       // Top accounts
       {
         path: 'top_accounts/:page?',
         name: 'top_accounts',
         component: TopAccounts,
+        meta: {
+          middleware: [language, network, page],
+        },
       },
       // Contracts
-      { path: 'contracts/:page?', name: 'contracts', component: Contracts },
+      {
+        path: 'contracts/:page?',
+        name: 'contracts',
+        component: Contracts,
+        meta: {
+          middleware: [language, network, page],
+        },
+      },
       // Assets
-      { path: 'assets/:page?', name: 'assets', component: Assets },
-      { path: 'asset/:id', name: 'asset', component: Asset },
+      {
+        path: 'assets/:page?',
+        name: 'assets',
+        component: Assets,
+        meta: {
+          middleware: [language, network, page],
+        },
+      },
+      {
+        path: 'asset/:id',
+        name: 'asset',
+        component: Asset,
+        meta: {
+          middleware: [language, network],
+        },
+      },
       /* Accounts group end */
 
       /* Governance group */
@@ -197,25 +291,64 @@ export default [
         path: 'protocol_amendment',
         redirect: { name: 'protocol_amendment' },
       },
-      { path: 'protocols', redirect: { name: 'protocols' } },
-      { path: 'period/:id', redirect: { name: 'period' } },
-      { path: 'vote/:voteHash', redirect: { name: 'vote' } },
+      {
+        path: 'protocols',
+        redirect: { name: 'protocols' },
+      },
+      {
+        path: 'period/:id',
+        redirect: { name: 'period' },
+      },
+      {
+        path: 'vote/:voteHash',
+        redirect: { name: 'vote' },
+      },
       /* Governance group end */
 
       /* Charts group */
-      { path: 'charts/baking', redirect: { name: 'baking_charts' } },
-      { path: 'charts/general', redirect: { name: 'general_charts' } },
+      {
+        path: 'charts/baking',
+        redirect: { name: 'baking_charts' },
+      },
+      {
+        path: 'charts/general',
+        redirect: { name: 'general_charts' },
+      },
       /* Charts group end */
 
-      { path: 'glossary', name: 'glossary', component: Glossary },
-      { path: 'feedback', name: 'feedback', component: Feedback },
-      { path: 'mempool', name: 'mempool', component: Mempool },
+      {
+        path: 'glossary',
+        name: 'glossary',
+        component: Glossary,
+        meta: {
+          middleware: [language, network],
+        },
+      },
+      {
+        path: 'feedback',
+        name: 'feedback',
+        component: Feedback,
+        meta: {
+          middleware: [language, network],
+        },
+      },
+      {
+        path: 'mempool',
+        name: 'mempool',
+        component: Mempool,
+        meta: {
+          middleware: [language, network],
+        },
+      },
 
       /* Errors group */
       {
         path: 'maintenance',
         name: 'maintenance',
         component: Maintenance,
+        meta: {
+          middleware: [language, network],
+        },
       },
       { path: '404', name: '404', component: NotFound },
       { path: '500', name: '500', component: ServerError },
@@ -233,10 +366,34 @@ export default [
         path: 'protocol_amendment',
         name: 'protocol_amendment',
         component: ProtocolAmendment,
+        meta: {
+          middleware: [language],
+        },
       },
-      { path: 'protocols', name: 'protocols', component: Protocols },
-      { path: 'period/:id', name: 'period', component: Period },
-      { path: 'vote/:voteHash', name: 'vote', component: Vote },
+      {
+        path: 'protocols',
+        name: 'protocols',
+        component: Protocols,
+        meta: {
+          middleware: [language],
+        },
+      },
+      {
+        path: 'period/:id',
+        name: 'period',
+        component: Period,
+        meta: {
+          middleware: [language],
+        },
+      },
+      {
+        path: 'vote/:voteHash',
+        name: 'vote',
+        component: Vote,
+        meta: {
+          middleware: [language],
+        },
+      },
       /* Governance group end */
 
       /* Charts group */
@@ -244,11 +401,17 @@ export default [
         path: 'charts/baking',
         name: 'baking_charts',
         component: ChartsBaking,
+        meta: {
+          middleware: [language],
+        },
       },
       {
         path: 'charts/general',
         name: 'general_charts',
         component: ChartsGeneral,
+        meta: {
+          middleware: [language],
+        },
       },
       /* Charts group end */
     ],
